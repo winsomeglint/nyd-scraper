04750229ccbd21115b275e1046a0d9eedc591b9f
lion summerbell <lion@smmbll.org>
2018-07-02 08:36:28 +0200

    Setuptools configuration

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name='nydis',
+    version='1.0',
+    packages=find_packages(),
+    scripts=['app.py'],
+    install_requires = ['requests', 'lxml', 'click'],
+    author='Lion Summerbell',
+    description='Scraper to download filers and disclosures from elections.ny.gov',
+    license='ISC',
+    keywords='ny state elections',
+    url='https://github.com/anabase/ny_disclosures',
+    entry_ponts='''
+        [console_scripts]
+        nydis=app:cli
+    ''',
+)
